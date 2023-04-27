e6f77efa1fb2062123dab1a318664a8d0fd842ac
nivesh <niveshg@hotmail.com>
2023-04-27 10:11:23 +0100

    L2 end + challenge

--- main.js
+++ main.js
@@ -1,1 +1,10 @@
-const product = 'Socks'
+const app = Vue.createApp({
+  data() {
+    return {
+      product: "Socks",
+      description: "A garment for the foot and lower part of the leg, typically knitted from wool, cotton, or nylon."
+    };
+  },
+});
+
+const mountedApp = app.mount("#app");
