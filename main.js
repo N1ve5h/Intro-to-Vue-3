4bca6e46d54ba91f6024add2ebeea0034be18f85
nivesh <niveshg@hotmail.com>
2023-04-27 10:18:47 +0100

    L3 initial

--- main.js
+++ main.js
@@ -2,7 +2,6 @@ const app = Vue.createApp({
   data() {
     return {
       product: "Socks",
-      description: "A garment for the foot and lower part of the leg, typically knitted from wool, cotton, or nylon."
     };
   },
 });
